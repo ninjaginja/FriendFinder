aae0dcef8e5df5be8fdab860e62684993968b0e3
Molly Wojcik <mollywojcik@gmail.com>
2018-03-03 08:25:31 -0800

    differences calculating correctly

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,19 +3,16 @@ var friends = [
     name: "JuneBug",
     imageUrl: "http://www.petwave.com/-/media/Images/Center/Breed/Dogs/Sporting-Group/Wirehaired-Pointing-Griffon/Wirehaired-Pointing-Griffon-2.ashx?w=450&hash=43A36F9D5C2616BAC7B4EB2CC26C6CCF1A5BCEA6",
     responses: [1, 5, 2, 3, 1, 3, 1, 2, 1, 1],
-    // routeName: "junebug"
   },
   {
     name: "Gretta Mae",
     imageUrl: "https://cmeimg-a.akamaihd.net/640/ppds/a63add51-0e78-4b78-a1e5-92cb51b61e12.jpg",
     responses: [1, 1, 4, 2, 5, 4, 1, 1, 2, 5],
-    // routeName: "grettamae"
   },
   {
     name: "Eli",
     imageUrl: "https://i.pinimg.com/736x/f7/0d/57/f70d57c9ebff225a68343502f63cd5e2--the-gray-grey.jpg",
     responses: [1, 4, 1, 1, 4, 4, 4, 5, 3, 5],
-    // routeName: "eli"
   }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,29 +1,31 @@
 var path = require("path");
-var friends = require("../data/friends");
+var friends = require("../data/friends.js");
 
 module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
+  // console.log(friends[0].responses);
 
   app.post("/api/friends", function(req, res) {
 
     var newFriend = req.body;
     // console.log(JSON.parse(newFriend.responses))
-    // console.log('newFriend = ' + JSON.stringify(newFriend));
-    // newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+    console.log('newFriend = ' + JSON.stringify(newFriend));
 
     var newResponses = newFriend.responses;
     console.log("New Responses = " + newResponses);
 
-    // for (var i = 0; i < friends.length; i++) {
-    //   var difference = 0;
-    //   for (var j = 0; j < newResponses.length; j++) {
-    //     difference += Math.abs(friends[i].newResponses[j] - responses[j]);
-    //   }
-    //   console.log(difference);
-    //
-    // }
+
+
+    for (var i = 0; i < friends.length; i++) {
+      var difference = 0;
+      for (var j = 0; j < newResponses.length; j++) {
+        difference += Math.abs(friends[i].responses[j] - newResponses[j]);
+      }
+      console.log(difference);
+
+    }
 
     friends.push(newFriend);
 
