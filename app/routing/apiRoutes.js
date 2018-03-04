643d96375c8a34b7c469059a84977e1c383071ce
Molly Wojcik <mollywojcik@gmail.com>
2018-03-04 07:28:53 -0800

    Modal is working.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,11 +11,14 @@ module.exports = function(app) {
 
     var newFriend = req.body;
     // console.log(JSON.parse(newFriend.responses))
-    console.log('newFriend = ' + JSON.stringify(newFriend));
+    // console.log('newFriend = ' + JSON.stringify(newFriend));
 
     var newResponses = newFriend.responses;
-    console.log("New Responses = " + newResponses);
+    // console.log("New Responses = " + newResponses);
 
+    var differenceCompare = 1000;
+    var matchName = "";
+    var matchImage = "";
 
 
     for (var i = 0; i < friends.length; i++) {
@@ -23,13 +26,22 @@ module.exports = function(app) {
       for (var j = 0; j < newResponses.length; j++) {
         difference += Math.abs(friends[i].responses[j] - newResponses[j]);
       }
-      console.log(difference);
-
+      // console.log(difference);
+      if (difference < differenceCompare) {
+        differenceCompare = difference;
+        matchName = friends[i].name;
+        matchImage = friends[i].imageUrl;
+      }
     }
+
+    // console.log(differenceCompare);
+    // console.log(matchName);
+    // console.log(matchImage);
 
     friends.push(newFriend);
 
-    res.json(newFriend);
+    // res.json(newFriend);
+    res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
 
   });
 
